27c4403f61c777c2a3ef922444ec91d204766e0e
BigMcQueen <mmmrym10@gmail.com>
2022-12-06 21:30:31 +0900

    web app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+
+# create the extension
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+# initialize the app with the extension
+db.init_app(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(300), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone("Asia/Tokyo")))
+
+@app.route("/")
+def index():
+    return render_template("index.html")

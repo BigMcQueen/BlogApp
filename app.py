dab865f8de4e780c8455aa7ef95cc49a9d9bd096
BigMcQueen <mmmrym10@gmail.com>
2022-12-11 20:59:00 +0900

    add css and bootstrap

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from flask_bootstrap import Bootstrap
 
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
@@ -17,6 +18,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 app.config["SECRET_KEY"] = os.urandom(24)
 # initialize the app with the extension
 db.init_app(app)
+bootstrap = Bootstrap(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)

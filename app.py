f4cf790f346d00872e84f622834d617b7cee1543
BigMcQueen <mmmrym10@gmail.com>
2022-12-11 16:10:56 +0900

    create signup and login function

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user
+
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from datetime import datetime
 import pytz
 
@@ -10,14 +14,23 @@ db = SQLAlchemy()
 app = Flask(__name__)
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SECRET_KEY"] = os.urandom(24)
 # initialize the app with the extension
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone("Asia/Tokyo")))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(12))
 
 with app.app_context():
     db.create_all()
@@ -27,6 +40,35 @@ def index():
     if request.method == "GET":
         posts = Post.query.all()
         return render_template("index.html", posts=posts)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User(username=username, password=generate_password_hash(password, method="sha256"))
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect("/login")
+
+    else:
+        return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect("/")
+
+    else:
+        return render_template("login.html")
 
 @app.route("/create", methods=["GET", "POST"])
 def create():
